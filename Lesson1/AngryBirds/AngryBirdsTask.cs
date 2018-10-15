de600edcf5a95f725bde5329d2f38c1333eada50
notenot <notsu@mail.ru>
2018-10-15 20:07:18 +0300

    Complete task 'Angry Birds'

--- Lesson1/AngryBirds/AngryBirdsTask.cs
+++ Lesson1/AngryBirds/AngryBirdsTask.cs
@@ -4,15 +4,14 @@ namespace AngryBirds
 {
 	public static class AngryBirdsTask
 	{
-		//  Ниже — это XML документация, её использует ваша среда разработки, 
-		// чтобы показывать подсказки по использованию методов. 
-		// Но писать её естественно не обязательно.
+        private static double g = 9.8;
+
 		/// <param name="v">Начальная скорость</param>
 		/// <param name="distance">Расстояние до цели</param>
 		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
 		public static double FindSightAngle(double v, double distance)
 		{
-			return Math.PI / 4;
+            return 0.5 * Math.Asin(distance * g / (v * v));
 		}
 	}
 }
