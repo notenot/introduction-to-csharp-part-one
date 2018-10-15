6b799be92ede024a9d42085317a3af0a0f067069
notenot <notsu@mail.ru>
2018-10-15 21:09:49 +0300

    Complete task 'Billiards'

--- Lesson1/Billards/BilliardsTask.cs
+++ Lesson1/Billards/BilliardsTask.cs
@@ -5,15 +5,16 @@ namespace Billiards
     public static class BilliardsTask
     {
         /// <summary>
-        /// 
+        /// Расчитывает угол отскока шара от стены без учета физических эффектов. 
         /// </summary>
         /// <param name="directionRadians">Угол направелния движения шара</param>
         /// <param name="wallInclinationRadians">Угол</param>
-        /// <returns></returns>
+        /// <returns>Угол отскока</returns>
         public static double BounceWall(double directionRadians, double wallInclinationRadians)
         {
-            //TODO
-            return 0.0;
+            var wallInclinationDegrees = wallInclinationRadians * 180 / Math.PI;
+            var directionDegrees = directionRadians * 180 / Math.PI;
+            return (360 - directionDegrees + 2 * wallInclinationDegrees) * Math.PI / 180.0;
         }
     }
 }
