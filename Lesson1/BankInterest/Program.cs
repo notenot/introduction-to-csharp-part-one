4e518dfa23df56f33ab53586ff27dbc89c802704
notenot <notsu@mail.ru>
2018-10-15 21:22:38 +0300

    Init task 'Bank Interest' from first lesson

---
+++ Lesson1/BankInterest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(Calculate(input));
+        }
+
+        public static double Calculate(string userInput)
+        {
+            return 0.0;
+        }
+    }
+}
