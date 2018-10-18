306561128f0e5e5d973dffba2240194ea3311eb2
notenot <notsu@mail.ru>
2018-10-18 15:06:24 +0300

    Complete task 'Pluralize'

--- Lesson2/Pluralize/PluralizeTask.cs
+++ Lesson2/Pluralize/PluralizeTask.cs
@@ -1,11 +1,24 @@
-﻿namespace Pluralize
+﻿using System.Linq;
+
+namespace Pluralize
 {
 	public static class PluralizeTask
 	{
 		public static string PluralizeRubles(int count)
 		{
-			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-			return "руб.";
+		    var lastTwoDigits = count % 100;
+		    if (new[] {11, 12, 13, 14}.Contains(lastTwoDigits))
+		        return "рублей";
+
+		    var lastDigit = count % 10;
+
+		    if (lastDigit == 1)
+		        return "рубль";
+
+		    if (new[] {2, 3, 4}.Contains(lastDigit))
+		        return "рубля";
+
+		    return "рублей";
 		}
 	}
 }
