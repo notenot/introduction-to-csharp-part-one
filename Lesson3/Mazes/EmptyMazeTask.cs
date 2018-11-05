8f9b600ad00f1747ba603406560f583e7f05613c
notenot <notsu@mail.ru>
2018-11-05 23:37:40 +0300

    Complete task 'Empty Maze'

--- Lesson3/Mazes/EmptyMazeTask.cs
+++ Lesson3/Mazes/EmptyMazeTask.cs
@@ -4,6 +4,20 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
+            MoveDown(robot, height - 3);
+            MoveRight(robot, width - 3);
 		}
-	}
+
+	    private static void MoveDown(Robot robot, int stepCount)
+	    {
+            for (var i = 0; i < stepCount; ++i)
+                robot.MoveTo(Direction.Down);
+	    }
+
+        private static void MoveRight(Robot robot, int stepCount)
+        {
+            for (var i = 0; i < stepCount; ++i)
+                robot.MoveTo(Direction.Right);
+        }
+    }
 }
