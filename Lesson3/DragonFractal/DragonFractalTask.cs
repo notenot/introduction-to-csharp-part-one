07e76714b8a8ab263882760a46e9a517c03424dd
notenot <notsu@mail.ru>
2018-11-07 21:55:55 +0300

    Complete task 'Dragon Fractal'

--- Lesson3/DragonFractal/DragonFractalTask.cs
+++ Lesson3/DragonFractal/DragonFractalTask.cs
@@ -1,4 +1,4 @@
-﻿// В этом пространстве имен содержатся средства для работы с изображениями. Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
+﻿using System;
 using System.Drawing;
 
 namespace Fractals
@@ -7,25 +7,49 @@ namespace Fractals
 	{
 		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
 		{
-			/*
-			Начните с точки (1, 0)
-			Создайте генератор рандомных чисел с сидом seed
-			
-			На каждой итерации:
+            var currentPixel = new Tuple<double, double>(1.0, 0.0);
+		    var random = new Random(seed);
 
-			1. Выберите случайно одно из следующих преобразований и примените его к текущей точке:
+            for (var i = 0; i < iterationsCount; ++i)
+            {
+                currentPixel = random.Next(2) == 0 ?
+                    PerformFirstTransformation(currentPixel) :
+                    PerformSecondTransformation(currentPixel);
 
-				Преобразование 1. (поворот на 45° и сжатие в sqrt(2) раз):
-				x' = (x · cos(45°) - y · sin(45°)) / sqrt(2)
-				y' = (x · sin(45°) + y · cos(45°)) / sqrt(2)
+                pixels.SetPixel(currentPixel.Item1, currentPixel.Item2);
+            }
+        }
 
-				Преобразование 2. (поворот на 135°, сжатие в sqrt(2) раз, сдвиг по X на единицу):
-				x' = (x · cos(135°) - y · sin(135°)) / sqrt(2) + 1
-				y' = (x · sin(135°) + y · cos(135°)) / sqrt(2)
-		
-			2. Нарисуйте текущую точку методом pixels.SetPixel(x, y)
+        /// <summary>
+        /// Выполняет поворот на 45° и сжатие в sqrt(2) раз.
+        /// </summary>
+        /// <param name="pixel">Начальная точка</param>
+        /// <returns>Точка, полученная в результате применения преобразования</returns>
+	    private static Tuple<double, double> PerformFirstTransformation(Tuple<double, double> pixel)
+	    {
+	        var x = pixel.Item1;
+	        var y = pixel.Item2;
+	        var angle = 45 * Math.PI / 180.0;
 
-			*/
-		}
+            return new Tuple<double, double>(
+                (x * Math.Cos(angle) - y * Math.Sin(angle)) / Math.Sqrt(2), 
+	            (x * Math.Sin(angle) + y * Math.Cos(angle)) / Math.Sqrt(2));
+        }
+
+        /// <summary>
+        /// Выполняет поворот на 135°, сжатие в sqrt(2) раз и сдвиг по X на единицу.
+        /// </summary>
+        /// <param name="pixel">Начальная точка</param>
+        /// <returns>Точка, полученная в результате применения преобразования</returns>
+        private static Tuple<double, double> PerformSecondTransformation(Tuple<double, double> pixel)
+	    {
+	        var x = pixel.Item1;
+	        var y = pixel.Item2;
+	        var angle = 135 * Math.PI / 180.0;
+
+	        return new Tuple<double, double>(
+	            (x * Math.Cos(angle) - y * Math.Sin(angle)) / Math.Sqrt(2) + 1,
+	            (x * Math.Sin(angle) + y * Math.Cos(angle)) / Math.Sqrt(2));
+        }
 	}
 }
