b5288332fca040c58c84c81057f831fbfb2a6880
notenot <notsu@mail.ru>
2018-11-13 15:49:52 +0300

    Complete task 'Heatmap'

--- Lesson4/Names/HeatmapTask.cs
+++ Lesson4/Names/HeatmapTask.cs
@@ -1,13 +1,29 @@
-﻿using System;
-
-namespace Names
+﻿namespace Names
 {
     internal static class HeatmapTask
     {
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
-            return new HeatmapData("Пример карты интенсивностей",
-                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, new[] { "a", "b", "c", "d" }, new[] { "X", "Y", "Z" });
+            var daysCount = 30;
+            var days = new string[daysCount];
+            for (var i = 0; i < daysCount; ++i)
+                days[i] = (i + 2).ToString();
+
+            var monthsCount = 12;
+            var months = new string[monthsCount];
+            for (var i = 0; i < monthsCount; ++i)
+                months[i] = (i + 1).ToString();
+
+            var heatmap = new double[daysCount, monthsCount];
+            foreach (var name in names)
+            {
+                if (name.BirthDate.Day == 1)
+                    continue;
+
+                ++heatmap[name.BirthDate.Day - 2, name.BirthDate.Month - 1];
+            }
+
+            return new HeatmapData("Карта интенсивностей", heatmap, days, months);
         }
     }
 }
