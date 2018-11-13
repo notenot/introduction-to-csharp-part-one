6cbcdf65842c9da703f72a9823da9dfc3cde1ec2
notenot <notsu@mail.ru>
2018-11-13 15:22:29 +0300

    Complete task 'Histogram'

--- Lesson4/Names/HistogramTask.cs
+++ Lesson4/Names/HistogramTask.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Linq;
-
-namespace Names
+﻿namespace Names
 {
     internal static class HistogramTask
     {
         public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
         {
-            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), new string[0], new double[0]);
+            var daysСount = 31;
+            var days = new string[daysСount];
+            for (var d = 0; d < daysСount; d++)
+                days[d] = (d + 1).ToString();
+
+            var birthsCounts = new double[daysСount];
+            foreach (var nameData in names)
+            {
+                if (nameData.Name != name || nameData.BirthDate.Day == 1)
+                    continue;
+
+                birthsCounts[nameData.BirthDate.Day - 1]++;
+            }
+
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), days, birthsCounts);
         }
     }
 }
