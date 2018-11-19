71125206c6392cf3d728095edaee062e50e70ccc
notenot <notsu@mail.ru>
2018-11-19 18:17:07 +0300

    Complete task 'Sentences Parser'

--- Lesson5/TextAnalysis/SentencesParserTask.cs
+++ Lesson5/TextAnalysis/SentencesParserTask.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -7,8 +8,42 @@ namespace TextAnalysis
         public static List<List<string>> ParseSentences(string text)
         {
             var sentencesList = new List<List<string>>();
-            //...
+            var possibleSentences = text.Split('.', '!', '?', ';', ':', '(', ')');
+            foreach (var possibleSentence in possibleSentences)
+            {
+                var sentence = ParseSentenceToWords(possibleSentence);
+                if (sentence == null)
+                    continue;
+                sentencesList.Add(sentence);
+            }
+
             return sentencesList;
+        }
+
+        private static List<string> ParseSentenceToWords(string sentence)
+        {
+            var sentenceList = new List<string>();
+            var wordBuilder = new StringBuilder();
+            foreach (var character in sentence)
+            {
+                if (char.IsLetter(character) || character == '\'')
+                    wordBuilder.Append(char.ToLower(character));
+                else
+                {
+                    if (wordBuilder.Length == 0)
+                        continue;
+
+                    sentenceList.Add(wordBuilder.ToString());
+                    wordBuilder.Clear();
+                }
+            }
+
+            if (wordBuilder.Length != 0)
+                sentenceList.Add(wordBuilder.ToString());
+
+            if (sentenceList.Count == 0)
+                return null;
+            return sentenceList;
         }
     }
 }
