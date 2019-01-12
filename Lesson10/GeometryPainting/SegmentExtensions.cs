e8ffaa159cb9cb8cf543cddcf9d10dae5b7c90b4
notenot <notsu@mail.ru>
2019-01-12 17:00:50 +0300

    Init task '256 Shades Of Grey' from tenth lesson

---
+++ Lesson10/GeometryPainting/SegmentExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+
+namespace GeometryPainting
+{
+    //Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+}
