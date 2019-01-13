c5295757ca57ea67c06dedf61a5b881082b57aec
notenot <notsu@mail.ru>
2019-01-13 21:46:50 +0300

    Complete task 'Sack And Gold'

--- Lesson11/Digger/DiggerTask.cs
+++ Lesson11/Digger/DiggerTask.cs
@@ -45,19 +45,19 @@ namespace Digger
             switch (key)
             {
                 case Keys.Up:
-                    if (y > 0) 
+                    if (y > 0 && !(Game.Map[x, y - 1] is Sack)) 
                         command.DeltaY = -1;
                     break;
                 case Keys.Down:
-                    if (y < Game.MapHeight - 1) 
+                    if (y < Game.MapHeight - 1 && !(Game.Map[x, y + 1] is Sack)) 
                         command.DeltaY = 1;
                     break;
                 case Keys.Left:
-                    if (x > 0) 
+                    if (x > 0 && !(Game.Map[x - 1, y] is Sack)) 
                         command.DeltaX = -1;
                     break;
                 case Keys.Right:
-                    if (x < Game.MapWidth - 1) 
+                    if (x < Game.MapWidth - 1 && !(Game.Map[x + 1, y] is Sack)) 
                         command.DeltaX = 1;
                     break;
             }
@@ -67,7 +67,74 @@ namespace Digger
 
         public bool DeadInConflict(ICreature conflictedObject)
         {
+            return conflictedObject is Sack;
+        }
+    }
+
+    public class Sack : ICreature
+    {
+        private int passedCells;
+
+        public string GetImageFileName()
+        {
+            return "Sack.png";
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 3;
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            var command = new CreatureCommand();
+            var isFalling = y < Game.MapHeight - 1 &&
+                (Game.Map[x, y + 1] is Player && passedCells > 0 || Game.Map[x, y + 1] == null);
+
+            if (isFalling)
+            {
+                command.DeltaY = 1;
+                ++passedCells;
+            }
+            else
+            {
+                if (passedCells > 1)
+                    command.TransformTo = new Gold();
+                passedCells = 0;
+            }
+
+            return command;
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
             return false;
+        }
+    }
+
+    public class Gold : ICreature
+    {
+        public string GetImageFileName()
+        {
+            return "Gold.png";
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 2;
+        }
+
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            var isConflictWithPlayer = conflictedObject is Player;
+            if (isConflictWithPlayer)
+                Game.Scores += 10;
+            return isConflictWithPlayer;
         }
     }
 }
