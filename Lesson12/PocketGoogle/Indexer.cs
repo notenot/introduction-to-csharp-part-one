5afb6f0d22e6b2278b368d0f517f30b93ac6423d
notenot <notsu@mail.ru>
2019-01-15 22:33:20 +0300

    Complete task 'Pocket Google'

--- Lesson12/PocketGoogle/Indexer.cs
+++ Lesson12/PocketGoogle/Indexer.cs
@@ -1,31 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PocketGoogle
 {
     public class Indexer : IIndexer
     {
+        private static readonly char[] delimiters;
+        // word <id, positions>
+        private Dictionary<int, string[]> notIndexedWords; 
+        private Dictionary<string, Dictionary<int, List<int>>> indexedWords;
+
+        static Indexer()
+        {
+            delimiters = new[] { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
+        }
+
+        public Indexer()
+        {
+            notIndexedWords = new Dictionary<int, string[]>();
+            indexedWords = new Dictionary<string, Dictionary<int, List<int>>>();
+        }
+
         public void Add(int id, string documentText)
         {
-            throw new NotImplementedException();
+            var words = documentText.Split(delimiters);
+            notIndexedWords.Add(id, words);
+
+            var position = 0;
+            foreach (var word in words)
+            {
+                if (indexedWords.ContainsKey(word))
+                    if (indexedWords[word].ContainsKey(id))
+                        indexedWords[word][id].Add(position);
+                    else
+                        indexedWords[word].Add(id, new List<int> { position });
+                else
+                {
+                    var positionsById = new Dictionary<int, List<int>>
+                        { {id, new List<int> { position } } };
+                    indexedWords.Add(word, positionsById);
+                }
+
+                position += word.Length + 1;
+            }
         }
 
-        public List<int> GetIds(string word)
-        {
-            throw new NotImplementedException();
-        }
+        public List<int> GetIds(string word) =>
+            indexedWords.ContainsKey(word) 
+                ? indexedWords[word].Keys.ToList() 
+                : new List<int>();
 
-        public List<int> GetPositions(int id, string word)
-        {
-            throw new NotImplementedException();
-        }
+        public List<int> GetPositions(int id, string word) =>
+            indexedWords.ContainsKey(word) && indexedWords[word].ContainsKey(id)
+                ? indexedWords[word][id]
+                : new List<int>();
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            if (!notIndexedWords.ContainsKey(id))
+                return;
+
+            foreach (var word in notIndexedWords[id])
+                indexedWords[word].Remove(id);
+
+            notIndexedWords.Remove(id);
         }
     }
 }
