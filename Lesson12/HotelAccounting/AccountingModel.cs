dbc8bd4e31d41ab2638a913fc6c920e822172a75
notenot <notsu@mail.ru>
2019-01-15 16:56:31 +0300

    Complete task 'Hotel Accouting'

--- Lesson12/HotelAccounting/AccountingModel.cs
+++ Lesson12/HotelAccounting/AccountingModel.cs
@@ -1,10 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotelAccounting
 {
-    //создайте класс AccountingModel здесь
+    public class AccountingModel : ModelBase
+    {
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                price = value;
+                Notify(nameof(Price));
+                Notify(nameof(Total));
+            }
+        }
+
+        private int nightsCount;
+
+        public int NightsCount
+        {
+            get { return nightsCount; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException();
+                nightsCount = value;
+                Notify(nameof(NightsCount));
+                Notify(nameof(Total));
+            }
+        }
+
+        private double discount;
+
+        public double Discount
+        {
+            get { return discount; }
+            set
+            {
+                if (value > 100) throw new ArgumentException();
+                discount = value;
+                Notify(nameof(Discount));
+                Notify(nameof(Total));
+            }
+        }
+
+        private double total;
+
+        public double Total
+        {
+            get { return price * nightsCount * (1 - discount / 100); }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                total = value;
+                Notify(nameof(Total));
+                discount = 100 * (1 - total / (price * nightsCount));
+                Notify(nameof(Discount));
+            }
+        }
+    }
 }
