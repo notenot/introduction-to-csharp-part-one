b0c5e068e2b22e3a16fb8a0d1a6b5e469c764d8d
notenot <notsu@mail.ru>
2019-01-16 15:58:43 +0300

    Complete task 'Chart Builder'

--- Lesson13/Profiling/ChartBuilder.cs
+++ Lesson13/Profiling/ChartBuilder.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
-using ZedGraph;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Profiling
 {
-	class ChartBuilder
+	class ChartBuilder : IChartBuilder
 	{
+	    public Control Build(List<ExperimentResult> result)
+	    {
+	        var chart = new Chart();
+	        chart.ChartAreas.Add(new ChartArea());
+            var structGraph = new Series();
+	        var classGraph = new Series();
 
+            foreach (var experiment in result)
+            {
+                structGraph.Points.Add(new DataPoint(experiment.Size, experiment.StructResult));
+                classGraph.Points.Add(new DataPoint(experiment.Size, experiment.ClassResult));
+            }
+
+            structGraph.ChartType = SeriesChartType.FastLine;
+	        structGraph.Color = Color.Red;
+
+	        classGraph.ChartType = SeriesChartType.FastLine;
+	        classGraph.Color = Color.Green;
+
+	        chart.Series.Add(structGraph);
+	        chart.Series.Add(classGraph);
+	        chart.Dock = DockStyle.Fill;
+	        return chart;
+        }
 	}
 }
