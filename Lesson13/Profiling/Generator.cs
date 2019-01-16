878eeb76eef50cd5f4d7980916ca66ee1521e0b5
notenot <notsu@mail.ru>
2019-01-16 14:12:09 +0300

    Init tasks 'Profiling' from thirteenth lesson

---
+++ Lesson13/Profiling/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profiling
+{
+    class Generator
+    {
+        public static string GenerateDeclarations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GenerateArrayRunner()
+        {
+            throw new NotImplementedException();
+        }
+        public static string GenerateCallRunner()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
