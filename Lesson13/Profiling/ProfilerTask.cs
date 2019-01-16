e41542bae6586ccba5d8ef4cdeb68f651e22ff94
notenot <notsu@mail.ru>
2019-01-16 15:31:18 +0300

    Complete task 'Profiler'

--- Lesson13/Profiling/ProfilerTask.cs
+++ Lesson13/Profiling/ProfilerTask.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Profiling
@@ -8,7 +7,32 @@ namespace Profiling
 	{
 		public List<ExperimentResult> Measure(IRunner runner, int repetitionsCount)
 		{
-			throw new NotImplementedException();
+		    var result = new List<ExperimentResult>();
+
+            foreach (var fieldCount in Constants.FieldCounts)
+		    {
+		        PerformExperiment(runner, true, fieldCount, 1);
+		        var classTime = PerformExperiment(runner, true, fieldCount, repetitionsCount);
+
+		        PerformExperiment(runner, false, fieldCount, 1);
+		        var structTime = PerformExperiment(runner, false, fieldCount, repetitionsCount);
+
+		        result.Add(new ExperimentResult(fieldCount, classTime, structTime));
+		    }
+
+            return result;
 		}
-	}
+
+	    private double PerformExperiment
+	        (IRunner runner, bool isClass, int fieldCount, int repetitionsCount)
+	    {
+	        var watch = new Stopwatch();
+
+	        watch.Start();
+	        runner.Call(isClass, fieldCount, repetitionsCount);
+	        watch.Stop();
+
+	        return (double)watch.ElapsedMilliseconds / repetitionsCount;
+        }
+    }
 }
