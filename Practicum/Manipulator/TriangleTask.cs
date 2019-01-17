9375a92c39f29bbb588932e3e6a9ed56e16728a4
notenot <notsu@mail.ru>
2019-01-17 16:45:00 +0300

    Complete task 'Angle Search'

--- Practicum/Manipulator/TriangleTask.cs
+++ Practicum/Manipulator/TriangleTask.cs
@@ -8,10 +8,12 @@ namespace Manipulation
         /// <summary>
         /// Возвращает угол (в радианах) между сторонами a и b в треугольнике со сторонами a, b, c 
         /// </summary>
-        public static double GetABAngle(double a, double b, double c)
-        {
-            return 0.0;
-        }
+        public static double GetABAngle(double a, double b, double c) =>
+            a < 0 || b < 0 || c < 0 ||
+            a + b < c || a + c < b || b + c < a ||
+            Math.Abs(a * b) < double.Epsilon
+                ? double.NaN
+                : Math.Acos((a* a + b* b - c* c) / (2 * a* b));
     }
 
     [TestFixture]
@@ -19,11 +21,14 @@ namespace Manipulation
     {
         [TestCase(3, 4, 5, Math.PI / 2)]
         [TestCase(1, 1, 1, Math.PI / 3)]
-        // добавьте ещё тестовых случаев!
-        public void TestGetABAngle(double a, double b, double c, double expectedAngle)
-        {
-            Assert.Fail("Not implemented yet");
-        }
+        [TestCase(1, 2, 3, Math.PI)]
+        [TestCase(2, 3, 1, 0)]
+        [TestCase(3, 1, 2, 0)]
+        [TestCase(0, 0, 0, double.NaN)]
+        [TestCase(1, 2, 4, double.NaN)]
+        [TestCase(2, 4, 1, double.NaN)]
+        [TestCase(4, 1, 2, double.NaN)]
+        public void TestGetABAngle(double a, double b, double c, double expectedAngle) =>
+            Assert.AreEqual(expectedAngle, TriangleTask.GetABAngle(a, b, c), 1e-5);
     }
-
 }
